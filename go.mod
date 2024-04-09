b588cb6d886c2c669e7d97ac00b819c5cd6182bf
Abhishek-Jain-1925 <jainabhishek1925@gmail.com>
2024-04-09 14:35:06 +0530

    [send]:Basic Server Setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Abhishek-Jain-1925/Go-Micro-Services
+
+go 1.21.6
+
+require github.com/go-chi/chi v1.5.5
--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
 func main(){
 	fmt.Println("Hello World !!")
 
+	router := chi.NewRouter()
+
+	router.Get("/hello", myHandler);
+
 	server := &http.Server{
 		Addr: ":1925",
-		Handler: http.HandlerFunc(myHandler),
+		Handler: router,
 	}
 	fmt.Println("Server Running on Port 1925...")
 	err := server.ListenAndServe()
